e1188c9e5f6756e0807570572046ec6ac6aefc6a
Conway Zhou <98416475+conzh345@users.noreply.github.com>
2026-05-17 16:34:48 -0400

    adding project to github

---
+++ install_dependencies.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Installing ML4T ETF Rotation project dependencies...
+echo.
+
+pip install yfinance fredapi pandas numpy lightgbm scikit-learn matplotlib seaborn pyfolio-reloaded
+
+echo.
+echo Done.
+pause
